89eb55e3c9a3a65cc5c0784399da653a2c5d52d7
Jesús Hurtado <jjhurtado016@gmail.com>
2021-07-13 12:51:23 -0400

    Configuración de cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ require('./db')
     //Settings
 app.set('port', process.env.PORT || 3000)
 app.use('/', express.static(__dirname + '/public/views'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+}) 
 app.use(express.json());;
 app.use(express.urlencoded({ extended: false }))
     //Routes
